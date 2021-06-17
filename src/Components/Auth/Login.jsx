c3e653e58251656ec2354a3a41a6a54827e0cb15
shashi <shashi@gmail.com>
2021-06-17 15:36:25 +0530

    css added

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-
+import "./Auth.css";
 const Login = () => {
   return (
     <section id="authBlock">
       <article>
-        <h1>login Page</h1>
+        <div>
+          <h1>Welcome to Voot!</h1>
+          <p>Please Login for a more personalized experience.</p>
+          <form>
+            <div className="form-group">
+              <input type="text" name="email" id="email" />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="form-group">
+              <input type="password" name="password" id="password" />
+              <label htmlFor="password">Password</label>
+            </div>
+
+            <div className="form-group">
+              <button>Login</button>
+            </div>
+          </form>
+        </div>
       </article>
     </section>
   );
--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,10 +1,52 @@
 import React from "react";
-
+import "./Auth.css";
 const Register = () => {
   return (
     <section id="authBlock">
       <article>
-        <h1>Register Page</h1>
+        <h1>Welcome to Voot!</h1>
+        <p>Please Sign Up or Login for a more personalized experience.</p>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="enter username"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="enter email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="enter password"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmpassword">Confirm password</label>
+            <input
+              type="password"
+              name="confirmpassword"
+              id="confirmpassword"
+              placeholder="confirm password"
+            />
+          </div>
+          <div className="form-group">
+            <button>Register</button>
+          </div>
+        </form>
       </article>
     </section>
   );
